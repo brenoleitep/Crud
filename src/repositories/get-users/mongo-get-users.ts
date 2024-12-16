a75c1705b3d16e7a14b05eeb33460cf51a49f68a
Brene <brendoleitepereira@gmail.com>
2024-12-16 15:49:21 -0300

    Fea: ensure GetUsersMongoRepository gets users from mongo

--- src/repositories/get-users/mongo-get-users.ts
+++ src/repositories/get-users/mongo-get-users.ts
@@ -1,13 +1,17 @@
 import { IGetUsersRepository } from "../../controllers/get-users/protocols";
-import { IUser } from "../../models/user";
+import { MongoClient } from "../../database/mongo";
+import { User } from "../../models/user";
 
 export class MongoGetUsersRepository implements IGetUsersRepository {
-  async getUsers(): Promise<IUser[]> {
-    return [{
-      firstName: "John",
-      lastName: "Doe",
-      email: "john@example.com",
-      password: "password123"
-    }]
+  async getUsers(): Promise<User[]> {
+    const users = await MongoClient.db
+    .collection<Omit<User, 'id'>>('users')
+    .find({})
+    .toArray();
+
+    return users.map(({_id, ...rest}) => ({
+      ...rest,
+      id: _id.toHexString()
+    }));
   }
 }
