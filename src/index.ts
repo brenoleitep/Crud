d37f838ae99536e2f860710da9c2ada4e21a1ad1
Brene <brendoleitepereira@gmail.com>
2024-12-16 14:07:14 -0300

    Feat: add express server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-console.log("hello world")
+import { config } from "dotenv";
+import express from "express";
+
+config();
+
+const app = express();
+
+const port = process.env.port || 8000;
+
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+})
+
+app.listen(port, () => {
+  console.log( "listening on port " + port)
+});
+
