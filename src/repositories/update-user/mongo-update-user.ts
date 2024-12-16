6e192af07e8162e9c79e782a31bcc331e50dc1c3
Brene <brendoleitepereira@gmail.com>
2024-12-16 18:59:41 -0300

    Feat: add MongoUser protocol

--- src/repositories/update-user/mongo-update-user.ts
+++ src/repositories/update-user/mongo-update-user.ts
@@ -2,6 +2,7 @@ import { ObjectId } from "mongodb";
 import { IUpdateUserRespository, UpdateUserParams } from "../../controllers/update-user/protocols";
 import { MongoClient } from "../../database/mongo";
 import { User } from "../../models/user";
+import { MongoUser } from "../mongo-protocols";
 
 export class MongoUpdateUserRespository implements IUpdateUserRespository {
   async updateUser(id:string, params: UpdateUserParams): Promise<User> {
@@ -12,7 +13,7 @@ export class MongoUpdateUserRespository implements IUpdateUserRespository {
     });
 
     const user = await MongoClient.db
-    .collection<Omit<User, "id">>('users')
+    .collection<MongoUser>('users')
     .findOne({_id: new ObjectId(id)});
 
     if (!user) {
